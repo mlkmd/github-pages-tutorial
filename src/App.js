b994428640db5069e836759e007f3b1a31c937cb
mlkmd <milkdromeda1021@gmail.com>
2024-11-29 16:33:43 +0900

    develop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">This is GitHub Pages tutorial app.</div>;
+  return <div className="App">This is GitHub Pages tutorial app.(develop)</div>;
 }
 
 export default App;
