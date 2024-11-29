dbb1246273c40f6c79f39b39069036e11b6ee598
mlkmd <milkdromeda1021@gmail.com>
2024-11-29 12:23:26 +0900

    GitHub Pages setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # github-pages-tutorial
 GitHub Pages チュートリアル
+
+## デプロイ
+
+```bash
+npm run build
+npx gh-pages -d build
+```
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">This is GitHub Pages tutorial app.</div>;
+}
+
+export default App;
